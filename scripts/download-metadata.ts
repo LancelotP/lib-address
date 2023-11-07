580f4d8b40ed8160e41f0c07e7437666c9c64601
Lancelot Prigent <lancelot@prigent.dev>
2023-11-07 10:15:07 +0100

    add types generation

--- scripts/download-metadata.ts
+++ scripts/download-metadata.ts
@@ -1,11 +1,17 @@
 import { writeFile } from "node:fs/promises";
-import { resolve, dirname } from "node:path";
+import { dirname, resolve } from "node:path";
 import { fileURLToPath } from "node:url";
 import axios from "axios";
 import axiosRetry from "axios-retry";
 
-import { CountryData, SubRegionData } from "../src/types";
 import { CountryCode } from "../src/codes";
+import {
+  LocalityType,
+  StateType,
+  SublocalityType,
+  ZipType,
+} from "../src/generated";
+import { CountryData, SubRegionData } from "../src/types";
 
 axiosRetry(axios, { retries: 3 });
 
@@ -13,8 +19,6 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const BASE_URL = "https://chromium-i18n.appspot.com/ssl-address";
-
-type CountryMetadata = any;
 
 async function getCountryCodes() {
   const response = await fetch(`${BASE_URL}/data`);
@@ -37,7 +41,7 @@ type JSONSubRegionData = {
 
 async function getCountrySubRegionData(
   id: string,
-  extraLocales: string[] = []
+  extraLocales: string[] = [],
 ): Promise<SubRegionData> {
   const url = `${BASE_URL}/${id}`;
 
@@ -46,15 +50,15 @@ async function getCountrySubRegionData(
     extraLocales.map(async (locale) => ({
       locale,
       data: await axios.get<JSONSubRegionData>(`${url}--${locale}`),
-    }))
+    })),
   );
 
   console.log(res.data.id);
 
   const sub_regions = await Promise.all(
     (res.data.sub_keys?.split("~") ?? []).map((key) =>
-      getCountrySubRegionData(`${res.data.id}/${key}`, extraLocales)
-    )
+      getCountrySubRegionData(`${res.data.id}/${key}`, extraLocales),
+    ),
   );
 
   const foo = {
@@ -63,7 +67,7 @@ async function getCountrySubRegionData(
       default: res.data.name ?? res.data.key,
       latin: res.data.lname,
       ...Object.fromEntries(
-        resLocales.map(({ locale, data }) => [locale, data.data.name])
+        resLocales.map(({ locale, data }) => [locale, data.data.name]),
       ),
     },
     zip: res.data.zip,
@@ -75,7 +79,7 @@ async function getCountrySubRegionData(
 }
 
 async function getCountryData<K extends string>(
-  countryCode: K
+  countryCode: K,
 ): Promise<CountryData> {
   const url = `${BASE_URL}/data/${countryCode}`;
 
@@ -90,8 +94,8 @@ async function getCountryData<K extends string>(
 
   const sub_regions = await Promise.all(
     (res.data.sub_keys?.split("~") ?? []).map((key) =>
-      getCountrySubRegionData(`${res.data.id}/${key}`, extraLocales)
-    )
+      getCountrySubRegionData(`${res.data.id}/${key}`, extraLocales),
+    ),
   );
 
   return {
@@ -117,10 +121,10 @@ async function getCountryData<K extends string>(
 async function downloadMetadata() {
   const countryCodes = await getCountryCodes();
 
-  const dict: Record<string, CountryMetadata> = {};
+  const dict: Record<string, CountryData> = {};
 
   const results = await Promise.all(
-    countryCodes.map((countryCode) => getCountryData(countryCode))
+    countryCodes.map((countryCode) => getCountryData(countryCode)),
   );
 
   for (const result of results) {
@@ -132,21 +136,13 @@ async function downloadMetadata() {
       writeFile(
         resolve(__dirname, "../countries", `${result.key}.json`),
         JSON.stringify(result, null, 2),
-        "utf-8"
-      )
-    )
+        "utf-8",
+      ),
+    ),
   );
 
-  await writeFile(
-    resolve(__dirname, "../src/codes.ts"),
-    [
-      `export const codes = ${JSON.stringify(Object.keys(dict), null, 2)};`,
-      "",
-      "export type CountryCode = (typeof codes)[number]",
-      "",
-    ].join("\n"),
-    "utf-8"
-  );
+  await persistCountryCodes(dict);
+  await persistGeneratedTypes(dict);
 
   return dict;
 }
@@ -167,10 +163,10 @@ type JSONCountryData = {
   zipex?: string;
   lang?: string;
   languages?: string;
-  zip_name_type?: string;
-  state_name_type?: string;
-  locality_name_type?: string;
-  sublocality_name_type?: string;
+  zip_name_type?: ZipType;
+  state_name_type?: StateType;
+  locality_name_type?: LocalityType;
+  sublocality_name_type?: SublocalityType;
   sub_keys?: string;
   sub_names?: string;
   sub_zips?: string;
@@ -184,3 +180,76 @@ type JSONCountryData = {
   sub_xzips?: string;
   sub_xrequires?: string;
 };
+
+async function persistCountryCodes(dict: Record<string, CountryData>) {
+  await writeFile(
+    resolve(__dirname, "../src/codes.ts"),
+    [
+      `export const codes = ${JSON.stringify(
+        Object.keys(dict),
+        null,
+        2,
+      )} as const;`,
+      "",
+      "export type CountryCode = (typeof codes)[number]",
+      "",
+    ].join("\n"),
+    "utf-8",
+  );
+}
+
+async function persistGeneratedTypes(dict: Record<string, CountryData>) {
+  const defaultCountryData = await getCountryData("ZZ");
+  const combinedDict = { ...dict, ZZ: defaultCountryData };
+
+  const stateTypes = Array.from(
+    new Set(
+      Object.values(combinedDict)
+        .map((data) => data.state_name_type)
+        .filter(Boolean),
+    ),
+  ).sort((a, b) => a!.localeCompare(b!));
+
+  const zipTypes = Array.from(
+    new Set(
+      Object.values(combinedDict)
+        .map((data) => data.zip_name_type)
+        .filter(Boolean),
+    ),
+  ).sort((a, b) => a!.localeCompare(b!));
+
+  const localityTypes = Array.from(
+    new Set(
+      Object.values(combinedDict)
+        .map((data) => data.locality_name_type)
+        .filter(Boolean),
+    ),
+  ).sort((a, b) => a!.localeCompare(b!));
+
+  const sublocalityTypes = Array.from(
+    new Set(
+      Object.values(combinedDict)
+        .map((data) => data.sublocality_name_type)
+        .filter(Boolean),
+    ),
+  ).sort((a, b) => a!.localeCompare(b!));
+
+  await writeFile(
+    resolve(__dirname, "../src/generated.ts"),
+    [
+      `export type StateType = ${stateTypes.map((v) => `"${v}"`).join(" | ")};`,
+      "",
+      `export type ZipType = ${zipTypes.map((v) => `"${v}"`).join(" | ")};`,
+      "",
+      `export type LocalityType = ${localityTypes
+        .map((v) => `"${v}"`)
+        .join(" | ")};`,
+      "",
+      `export type SublocalityType = ${sublocalityTypes
+        .map((v) => `"${v}"`)
+        .join(" | ")};`,
+      "",
+    ].join("\n"),
+    "utf-8",
+  );
+}
--- src/registry.ts
+++ src/registry.ts
@@ -1,8 +1,22 @@
 import type { CountryCode } from "./codes";
 import { CountryMissingError } from "./errors/country-missing.error";
+import type {
+  LocalityType,
+  StateType,
+  SublocalityType,
+  ZipType,
+} from "./generated";
 import type { CountryData } from "./types";
 
-const defaultCountryData = {
+const defaultCountryData: {
+  fmt: string;
+  require: string;
+  upper: string;
+  zip_name_type: ZipType;
+  state_name_type: StateType;
+  locality_name_type: LocalityType;
+  sublocality_name_type: SublocalityType;
+} = {
   fmt: "%N%n%O%n%A%n%C",
   require: "AC",
   upper: "C",
@@ -17,7 +31,20 @@ type DefaultCountryData = typeof defaultCountryData;
 export const registry = new Map<CountryCode, CountryData>();
 
 export function registerCountry(
-  data: Omit<CountryData, "key"> & { key: string },
+  data: Omit<
+    CountryData,
+    | "key"
+    | "zip_name_type"
+    | "state_name_type"
+    | "locality_name_type"
+    | "sublocality_name_type"
+  > & {
+    key: string;
+    zip_name_type?: string;
+    state_name_type?: string;
+    locality_name_type?: string;
+    sublocality_name_type?: string;
+  },
 ) {
   registry.set(
     data.id.slice("data/".length) as CountryCode,
